488c3ec89c947fa11bf3ed4f6962ee122be5978d
AlvaroFerMaestro <141323952+AlvaroFerMaestro@users.noreply.github.com>
2024-11-16 18:12:04 +0100

    Add files via upload

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL)
+        console.log("conectado a la base de datos");
+    } catch (error) {
+        console.log("error al conectarse a la base de datos");
+    }
+    
+}
+
+module.exports = { connectDB }
